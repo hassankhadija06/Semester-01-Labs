d22ba627ec0dc2baedcf76c1bb83ccff12f85efa
Syeda Khadija Hassan <hassankhadija06@gmail.com>
2025-03-24 19:51:53 +0500

    Add files via upload

---
+++ Lab 03.cpp
@@ -0,0 +1,101 @@
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+//	double name1;
+//	name1 = 12.95;
+//	double name2;
+//	name2 = 24.95;
+//	double name3;
+//	name3 = 6.95;
+//	double name4;
+//	name4 = 14.95;
+//	double name5;
+//	name5 = 3.95;
+//	double subtotal = name1+name2+name3+name4+name5;
+//	double tax = subtotal*6/100;
+//	double total = subtotal+tax;
+//	cout<<"Price of item 1:"<<" "<<'$'<<name1<<endl;
+//	cout<<"Price of item 2:"<<" "<<'$'<<name2<<endl;
+//	cout<<"Price of item 3:"<<" "<<'$'<<name3<<endl;
+//	cout<<"Price of item 4:"<<" "<<'$'<<name4<<endl;
+//	cout<<"Price of item 5:"<<" "<<'$'<<name5<<endl;
+//	cout<<"Subtotal:"<<" "<<'$'<<subtotal<<endl;
+//	cout<<"Sales Tax (6%):"<<" "<<'$'<<tax<<endl;
+//	cout<<"Total:"<<" "<<'$'<<total<<endl;
+
+	
+//	double num1;
+//	num1 = 85.5;
+//	double num2;
+//	num2 = 90.75;
+//	double num3;
+//	num3 = 88.25;
+//	int totalnum;
+//	totalnum = 3;
+//	double sum = num1+num2+num3;
+//	double average = sum/totalnum;
+//	cout<<"The average of "<<num1<<","<<num2<<" "<<"and"<<" "<<num3<<" "<<"is"<<fixed<<setprecision(2)<<" "<<average<<endl;
+
+// MI<<endl;
+// int weight;
+// double height;
+// cout<<"BMI Calculator"<<endl;
+// cout<<"--------------"<<endl;
+// cout<<"Enter your weight in kilograms:";
+// cin>>weight;
+// cout<<"Enter your height in meters:";
+// cin>>height;
+// double height2;
+// height2= height*height;
+// double BMI = weight/height2;
+// cout<<"Your BMI is:"<<B
+// int num1,num2;
+// cout<<"ARITHMETIC OPERTAORS"<<endl;
+// cout<<"--------------------"<<endl;
+// cout<<"Enter first number:";
+// cin>>num1;
+// cout<<"Enter second number:";
+// cin>>num2;
+// cout<<"Number 1 :"<<num1<<endl;
+// cout<<"Number 2 :"<<num2<<endl;
+// int sum = num1+num2;
+// int difference = num1-num2;
+// int product = num1*num2;
+// int quotient = num1/num2;
+// int modulus = num1%num2;
+// cout<<"                "<<"Addition:"<<endl;
+// cout<<"                             "<<num1<<"+"<<num2<<"="<<sum<<endl;
+// cout<<"                "<<"Subtraction:"<<endl;
+// cout<<"                             "<<num1<<"-"<<num2<<"="<<difference<<endl;
+// cout<<"                "<<"Multiplication:"<<endl;
+// cout<<"                             "<<num1<<"*"<<num2<<"="<<product<<endl;
+// cout<<"                "<<"Division:"<<endl;
+// cout<<"                             "<<num1<<"/"<<num2<<"="<<quotient<<endl;
+// cout<<"                "<<"Modulus"<<endl;
+// cout<<"                             "<<num1<<"%"<<num2<<"="<<modulus<<endl;
+
+int distance;
+int vehiclefueleff;
+int fuelcost;
+cout<<"Trip Cost Estimator"<<endl;
+cout<<"-------------------"<<endl;
+cout<<"Enter distance of the trip in kilometers:"<<" ";
+cin>>distance;
+cout<<"Enter vehicle's fuel efficiency (km per liter):"<<" ";
+cin>>vehiclefueleff;
+cout<<"Enter fuel cost per liter:"<<" ";
+cin>>fuelcost;
+int totalcost;
+totalcost= distance/vehiclefueleff*fuelcost;
+cout<<"Total trip cost:"<<"Rs "<<totalcost<<endl;
+
+
+ 
+}
+	
+	
+	
+	
