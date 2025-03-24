6758e91d8253e9dbf8c2da9852541d18f07b428b
Syeda Khadija Hassan <hassankhadija06@gmail.com>
2025-03-24 19:57:04 +0500

    Add files via upload

---
+++ lab04.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+//	cout<<"EVEN ODD CLASSIFIER"<<endl;
+//	cout<<"-------------------"<<endl;
+//  int num1;
+//  cout<<"Enter an integer :";
+//  cin>>num1;
+//  (num1%2==0)?cout<<"The number "<<num1<<" is even\n":cout<<"The number "<<num1<<" is odd";
+//return 0;
+
+cout<<"AGE GROUP CLASSIFIER"<<endl;
+cout<<"--------------------"<<endl;
+int age;
+cout<<"Enter your age :";
+cin>>age;
+(age<13)?cout<<"Age"<<age<<" falls under the category : Child":
+(age>=13&&age<=19)?cout<<"Age"<<age<<" falls under the category : Teenager":
+cout<<"Age"<<age<<" falls under the category: Adult";
+return 0;
+
+
+
+//cout<<"PORTABLE BANKING APP"<<endl;
+//cout<<"--------------------"<<endl;
+//cout<<"Entrust us with your Finances!"<<endl;
+//cout<<endl;
+//int accno;
+//int balance;
+//bool loyalty;
+//cout<<"Enter your account number: ";
+//cin>>accno;
+//cout<<"Enter your account balance: $";
+//cin>>balance;
+//cout<<"Are you our loyal customer? \n(Enter 1 if Yes and 0 if No): ";
+//cin>>loyalty;
+//cout<<"Account Type\tLoyalty status\tSpecial offer eligibility"<<endl;
+//(balance<100)?cout<<"Low Balance Account\t":(balance>=100&&balance<=500)?cout<<"Standard Account\t":cout<<"Premium Account\t";
+//(loyalty==1)?cout<<"Loyal\t":cout<<"Not loyal\t";
+//(balance>200&&loyalty==1)?cout<<"\t\tEligible":cout<<"\t\tNot Eligible";
+//return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
